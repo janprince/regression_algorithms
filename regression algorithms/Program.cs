5b38b19ef94003b9fb9053b6f1dbfe0f7f60e451
Prince Jan Kumi <janprince002@gmail.com>
2022-07-18 10:24:26 +0000

    Add code for calculating dot product on arrays

--- regression algorithms/Program.cs
+++ regression algorithms/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿namespace regression_algorithms
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            double[] f = { 1.2 };
+            double[] s = { 1, 2 };
+
+            double result = VectorOperation.DotProduct(f, s);
+        }
+    }
+}
---
+++ regression algorithms/VectorOperation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace regression_algorithms
+{
+    internal class VectorOperation
+    {
+        // class to handle perform vector operations on C# arrays.
+
+        public static double DotProduct(double[] firstArray, double[] secondArray)
+        {
+            // returns a scalar, the result of a dot product.
+
+
+            double resultArray = 0;
+
+            // check if both arrays have the same length
+            if (firstArray.Length != secondArray.Length)
+            {
+                throw new ArgumentException("Length of Arrays do not much");
+            }
+            else
+            {
+                // perform the dot operation
+                for (int i = 0; i < firstArray.Length; i++)
+                {
+                    resultArray += (firstArray[i] * secondArray[i]);
+                }
+            }
+            return resultArray;
+        }
+    }
+}
